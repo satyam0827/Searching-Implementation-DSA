0b967eb04a043938432d1410a6888d4235ba1b6b
Satyam kumar <jhajhasatyam100@gmail.com>
2024-10-03 23:24:46 +0530

    add comment in binarysearch

--- binarSearch.java
+++ binarSearch.java
@@ -1,23 +1,28 @@
 package searching;
 
 public class binarSearch {
+
+    //binary search function
     public static int binaryS(int[] arr, int target){
         int left = 0,right = arr.length-1;
         while (left<=right) {
-            int mid = left+(right-left)/2;
+
+            int mid = left+(right-left)/2;  //formula to find mid 
             if(arr[mid]==target){
                 return mid;
             }
             if(arr[mid]<target){
-                left = mid+1;
+                left = mid+1; //shifting left to mid+1 if target is greater than mid ele
             }else{
-                right = mid-1;
+                right = mid-1; //shifting right to mid-1 if target is less than mid ele 
             }
         }
         return -1;
     }
+
+    //main function
     public static void main(String[] args) {
-        int[] arr = {1,2,10,25,35,50,65,78};
+        int[] arr = {1,2,10,25,35,50,65,78}; 
         int target = 65;
         System.out.println(binaryS(arr,target));
     }
